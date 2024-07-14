9091b456b62563a68f665cd88c5594eff06501f6
Jackson Gravino <jacksonjlgravino@gmail.com>
2024-07-14 10:35:06 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+const swiper = new Swiper(".slider-wrapper", {
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 30,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    610: {
+      slidesPerView: 2,
+    },
+    910: {
+      slidesPerView: 3,
+    },
+  },
+});
